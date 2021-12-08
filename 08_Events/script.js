7d7f77b3065ca1ceb22ffef671e550c93bd62b21
Owannnn <73163719+Owannnn@users.noreply.github.com>
2021-12-08 12:38:04 +0700

    Add files via upload

---
+++ 08_Events/script.js
@@ -0,0 +1,21 @@
+//1. query target node
+const okBtn = document.querySelector('#submitOK');
+const divBtn = document.querySelector('#divButtonPanel');
+
+//2. register event handler to target node
+okBtn.addEventListener('click', showMessage, true);
+divBtn.addEventListener(
+  'click',
+  () => {
+    alert('div click!!');
+  },
+  true
+);
+
+// okBtn.onclick = null;
+
+//3. prepare event handler
+function showMessage() {
+  alert('Good bye');
+}
+
